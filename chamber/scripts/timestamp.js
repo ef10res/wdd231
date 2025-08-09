ab7c17cfd681b91b847bb01f3c364de114f71dfb
ef10res <floreserick10@gmail.com>
2025-08-08 20:06:18 -0600

    timestamp

--- chamber/scripts/timestamp.js
+++ chamber/scripts/timestamp.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var timestampField = document.getElementById('timestampField');
     if (timestampField) {
         // Get the current timestamp (milliseconds since Unix epoch)
-        var timestamp = Date.now().toISOString();
+        var timestamp = Date.now().toString();
        
         timestampField.value = timestamp;
     }
